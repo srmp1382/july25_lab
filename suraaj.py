794720589f156d00d4749b647c9ec7af96ac6367
srmp1382 <109716654+srmp1382@users.noreply.github.com>
2025-07-30 05:58:04 +0530

    Update suraaj.py

--- suraaj.py
+++ suraaj.py
@@ -1,9 +1,13 @@
 # Simple greeting program
 
 # Ask for the user's name
+
 name = input("What is your name? ")
 
 # Print a greeting
 print("Hello, " + name + "! Nice to meet you, July 25 Batch.")
 
 print("ADD TO CART!")
+
+print("This is Sridhar\nI tried to access and put this message on 30th July around 6am for learning and testing")
+
